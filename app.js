eda171ed73a7898d87ad4f5e9654df29a1b6042e
Bjarni <bthe6@hi.is>
2021-06-11 17:11:35 +0000

    fix
    Fix

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ var usersRouter = require("./routes/users")
 
 var app = express()
 
-//app.use(express.static(path.join(__dirname, "build")))
-
-//app.get("/react", (req, res) => {
-//  res.sendFile(path.join(__dirname, "build", "index.html"))
-//})
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "jade")
